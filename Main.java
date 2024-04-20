1e7acaa876033283ff188b696247c9c5c9008679
Ritik Arora <22ritik22arora22@gmail.com>
2024-04-20 13:42:55 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("This is a Java Program");
+        System.out.println("Output from Java Program");
+        System.out.println(new Date());
+        
+    }
+}
