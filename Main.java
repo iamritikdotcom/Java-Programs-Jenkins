3add7cc6f20aafee21b56a4f0e690b7184375607
Ritik Arora <22ritik22arora22@gmail.com>
2024-04-21 01:29:42 +0530

    added one more line

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public class Main{
         System.out.println("This is a Java Program");
         System.out.println("Output from Java Program");
         System.out.println("Hello");
+        System.out.println("Added one more line");
     }
 }
